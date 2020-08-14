9e6f42db2608709773af3de1a1dd18f3807a22b7
zyx21 <3064662835@qq.com>
2020-08-14 14:32:08 +0800

    [feat]我的主页界面

--- GithubPopular/js/pages/Mypage.js
+++ GithubPopular/js/pages/Mypage.js
@@ -1,20 +1,125 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import {MoreMenu} from '../public/MoreMenu';
+import NavigatorBar from '../public/NavigatorBar';
+import SafeAreaViewPlus from '../public/SafeAreaViewPlus';
+import GlobalStyles from '../res/styles/GlobalStyles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ViewUtils from '../utils/ViewUtils';
+import NavigationUtils from '../utils/NavigationUtils';
 
 export default class MyPage extends React.Component {
+    onClick(menu) {
+        const {navigation} = this.props;
+        let routeName, params = {};
+        switch(menu) {
+            case MoreMenu.About:
+                routeName = 'AboutPage';
+                break;
+            default: 
+                break;
+        }
+        if (routeName) {
+            NavigationUtils.goPage(navigation, routeName, params);
+        }
+    }
+
+    getItem(menu) {
+        return ViewUtils.getMenuItem(() => this.onClick(menu), menu, '#678');
+    }
+
     render() {
+        let navigartor = <NavigatorBar title={'我的'}/>;
+        let content = <ScrollView>
+            <TouchableOpacity 
+                onPress={() => this.onClick(MoreMenu.About)}
+                style={styles.item}
+            >
+                <View style={styles.about_left}>
+                    <Ionicons 
+                        name={MoreMenu.About.icon}
+                        size={40}
+                        style={{marginRight: 40, color: '#678'}}
+                    />
+                    <Text>Github Popular</Text>
+                </View>
+                <Ionicons 
+                    name={'ios-arrow-forward'}
+                    size={16}
+                    style={{marginLeft: 10, alignSelf: 'center', color: '#678'}}
+                />
+            </TouchableOpacity>
+            <View style={GlobalStyles.line}/>
+
+            {this.getItem(MoreMenu.Tutorial)}
+            <View style={GlobalStyles.line}/>
+
+            {/* {趋势管理} */}
+            <Text style = {styles.groupTitle}>趋势管理</Text>
+            {/* {自定义语言} */}
+            {this.getItem(MoreMenu.Custom_Language)}
+            <View style={GlobalStyles.line}/>
+            {/* {语言排序} */}
+            {this.getItem(MoreMenu.Sort_Language)}
+
+            {/* {最热管理} */}
+            <Text style = {styles.groupTitle}>最热管理</Text>
+            {/* {自定义标签} */}
+            {this.getItem(MoreMenu.Custom_Key)}
+            {/* {标签排序} */}
+            <View style={GlobalStyles.line}/>
+            {this.getItem(MoreMenu.Sort_Key)}
+            {/* {标签移除} */}
+            <View style={GlobalStyles.line}/>
+            {this.getItem(MoreMenu.Remove_Key)}
+
+            {/* {设置管理} */}
+            <Text style = {styles.groupTitle}>设置</Text>
+            {/* {自定义主题} */}
+            {this.getItem(MoreMenu.Custom_Theme)}
+            {/* {关于作者} */}
+            <View style={GlobalStyles.line}/>
+            {this.getItem(MoreMenu.About_Author)}
+            {/* {反馈} */}
+            <View style={GlobalStyles.line}/>
+            {this.getItem(MoreMenu.Feedback)}
+            {/* {CodePush} */}
+            <View style={GlobalStyles.line}/>
+            {this.getItem(MoreMenu.CodePush)}
+        </ScrollView>;
         return (
-            <View style = {styel.container}>
-                <Text>Mine</Text>
-            </View>
+            <SafeAreaViewPlus topColor={'blue'}>
+                {navigartor}
+                {content}
+            </SafeAreaViewPlus>
         );
     }
 }
 
-const styel = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+    },
+
+    about_left: {
+        flexDirection: 'row',
         alignItems: 'center',
     },
+
+    item: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
+
+    groupTitle: {
+        marginLeft: 10, 
+        marginTop: 10, 
+        marginBottom: 5, 
+        fontSize: 12, 
+        color: 'gray',
+    }
 });
---
+++ GithubPopular/js/res/styles/GlobalStyles.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+
+const BACKGROUND_COLOR = '#f3f3f4';
+const window = Dimensions.get('window');
+
+export default {
+    line: {
+        height: 0.5,
+        opacity: 0.5,
+        backgroundColor: 'darkgray',
+    },
+    root_container: {
+        flex: 1,
+        backgroundColor: BACKGROUND_COLOR,
+    },
+    backgroundColor: BACKGROUND_COLOR,
+    nav_bar_height_ios:  44,
+    nav_bar_height_android: 50,
+    window_height: window.height,
+}
